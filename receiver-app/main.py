66528c17328f3debd84a36e28e7a113decfd50ae
suzdalenko-dev <alexey.suzdalenko@gmail.com>
2026-09-01 13:40:37 +0200

    suzdalenko

--- receiver-app/main.py
+++ receiver-app/main.py
@@ -1,4 +1,38 @@
-from sfunctions.func import config_app
+import paho.mqtt.client as mqtt
+from sfunctions.func import MQTT_TOPIC, MQTT_CLIENT, MQTT_USER, MQTT_PASS, MQTT_HOST, MQTT_PORT
+
+def on_connect(client, userdata, connect_flags, reason_code, properties):
+    if reason_code == 0:
+        print(f'CONNECTADO')
+        client.subscribe(MQTT_TOPIC, qos=1)
+        print(f'escuchando topic {MQTT_TOPIC}')
+    else:
+        print(f'ERROR {reason_code}')
 
 
-print(config_app)
+def on_desconnect(client, userdata, disconnect_flags, reason_code, propeties):
+    if reason_code != 0:
+        print(f'Conexion MQTT perdidida {reason_code}')
+
+
+def on_message(client, userdata, message):
+    try:
+        payload = message.payload.decode("utf-8")
+        print(f'{message}')
+
+    except Exception:
+        print(f'ERROR {str(message)}')
+
+
+
+client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2, client_id=MQTT_CLIENT, protocol=mqtt.MQTTv311)
+client.username_pw_set(username=MQTT_USER, password=MQTT_PASS)
+client.on_connect = on_connect
+client.on_disconnect = on_desconnect
+client.on_message = on_message
+client.reconnect_delay_set(min_delay=2, max_delay=33)
+client.connect_async(host=MQTT_HOST, port=MQTT_PORT, keepalive=66)
+client.loop_forever(retry_first_connection=True)
+
+
+
--- receiver-app/sfunctions/func.py
+++ receiver-app/sfunctions/func.py
@@ -4,3 +4,12 @@ from dotenv import dotenv_values
 base_dir    = Path(__file__).resolve().parent.parent.parent
 config_file = base_dir / "config" / "mqtt-receiver.env"
 config_app  = dotenv_values(config_file)
+
+
+MQTT_HOST   = config_app.get('HOST')
+MQTT_PORT   = config_app.get('PORT')
+MQTT_USER   = config_app.get('USER')
+MQTT_PASS   = config_app.get('PASS')
+MQTT_TOPIC  = config_app.get('TOPIC')
+MQTT_CLIENT = config_app.get('CLIENT')
+MQTT_LOG    = config_app.get('LOGURL')
